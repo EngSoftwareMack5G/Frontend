f651a2bee600990b78692321839eb84079578444
Riccio <riccio.fernando@gmail.com>
2025-05-12 18:58:27 -0300

    Minor fixes

--- routers/front/mentorado.py
+++ routers/front/mentorado.py
@@ -31,13 +31,7 @@ async def inscricao(request: Request):
 
 @router.get("/mentorship/{mentorship_id}", response_class=HTMLResponse)
 async def mentorship_detail(request: Request, mentorship_id: int):
-    return templates.TemplateResponse(
-        "Mentorado/mentorship.html",
-        {
-            "request": request,
-            "mentorship_id": mentorship_id
-        }
-    )
+    return templates.TemplateResponse("Mentorado/mentorship.html", {"request": request,})
 
 
 @router.get("/my_mentorships", response_class=HTMLResponse)
